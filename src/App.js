cc4562cacc3781c0f3252dd890b4f3e3ff68200f
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2024-05-18 20:47:29 +0100

    Replaced React template with simple flex layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header className="header">
+        <nav>
+          <ul className="navigation">
+            <li>Login</li>
+            <li>Logout</li>
+          </ul>
+        </nav>
       </header>
-    </div>
+      <main className="main">Hello</main>
+    </>
   );
 }
 
