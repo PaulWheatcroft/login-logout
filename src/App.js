1a1f0acd97df6e567d686b3f4af76d700bdb7033
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2024-05-18 21:40:28 +0100

    React router and HomePage component added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import HomePage from "./components/HomePage/HomePage";
 import LoginForm from "./components/LoginForm/LoginForm";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <header className="header">
-        <nav>
-          <ul className="navigation">
-            <li>Login</li>
-            <li>Logout</li>
-          </ul>
-        </nav>
-      </header>
-      <main className="main">
-        <LoginForm />
-      </main>
-    </>
+    <Router>
+      <>
+        <header className="header">
+          <nav>
+            <ul className="navigation">
+              <li>Login</li>
+              <li>Logout</li>
+            </ul>
+          </nav>
+        </header>
+        <main className="main">
+          <div className="content">
+            <Routes>
+              <Route path="/" exact element={<HomePage />} />
+              <Route path="/login" element={<LoginForm />} />
+            </Routes>
+          </div>
+        </main>
+      </>
+    </Router>
   );
 }
 
---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,11 @@
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>Welcome to the world of Login Logout</h1>
+    </>
+  );
+};
+
+export default HomePage;
