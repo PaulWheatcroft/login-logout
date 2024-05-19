dd35eb7bcf04376691cb671a4622d7bc340491ff
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2024-05-19 22:00:13 +0100

    Navbar text alignment css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 import React, { useContext } from "react";
 import { UserContext } from "../../context/UserContext";
 import Logout from "../Logout/Logout";
+import "./Header.css";
 
 function Header() {
   const { user } = useContext(UserContext);
   return (
     <header className="header">
-      <nav>
+      <nav className="navbar">
         <ul className="navigation">
           <li>
             <Link to="/">Home</Link>
